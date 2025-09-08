d5e3daaee6e53161a28524b210c5852c89ab7c84
Harsh Bhardwaj <89222581+Harshsfd@users.noreply.github.com>
2025-09-08 23:16:44 +0530

    Update server.ts

--- Backend/src/server.ts
+++ Backend/src/server.ts
@@ -1,6 +1,10 @@
-import app from './app';
+// Backend/src/server.ts
+import app from "./app";
+import dotenv from "dotenv";
 
-const PORT = process.env.PORT || 5000;
+dotenv.config();
+
+const PORT = process.env.PORT ? Number(process.env.PORT) : 5000;
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
